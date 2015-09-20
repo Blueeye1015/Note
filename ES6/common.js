860697a2f9d13db77e7c9045acbdbe4edd30a91a
Blueeye1015 <d.ebony.ivory@gmail.com>
2015-09-20 11:48:53 +0800

    update notes

--- ES6/common.js
+++ ES6/common.js
@@ -33,9 +33,71 @@
 /*
 	4.对象解构赋值
 	与数组的区别：顺序不必相同，属性名必须相同
+	对一个已经声明的变量(对象)用于解构赋值要小心，见例3
 */
 {
 	let {a, b} = {a: 1, b: 2};
+	var {x, y = 5} = {x: 1}; //这个默认值有点怪
+	({foo} = {foo: 1});
+}
+
+/*
+	5.字符串解构赋值
+*/
+{
+	const [a, b, c, d, e] = 'hello';
+}
+
+/*
+	6.函数参数的解构赋值
+
+	小结解构赋值用途:
+		* 交换变量的值
+		* 从函数返回多个值
+		* 函数参数的定义
+		* 提取JSON数据
+		* 遍历MAP结构
+		* 输入指定模块方法
+*/
+{
+	function add([x, y]) {
+		return x + y;
+	}
+	add([1, 2]);
+}
+
+/*
+	7.字符串的扩展
+	可以用``来输入字符串模板，并且可以保留空格和缩进
+	``内可以调用变量${var}
+*/
+{
+	let a = '\u{20BB7}';
+	let s = '吉a';
+	let string = 'Hello world!';
+
+	String.fromCodePoint(0x20BB7);
+
+	s.codePointAt(0); // 134071
+
+	'abc'.at(0); // a
+	'吉林'.at(0); // 吉
+
+	string.startsWith('Hello');
+	string.endsWith('!');
+	string.includes('o');
+
+	'haha'.repeat(2); // 'hahahaha'
+}
+
+/*
+	8.标签模板
+*/
+{
+	let a = 5;
+	let b = 10;
+
+	tag`Hello ${a + b} world ${a * b}`;
 }
 
 // arrow function
--- ES6/compiled.js
+++ ES6/compiled.js
@@ -6,6 +6,12 @@
 */
 'use strict';
 
+var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+
+var _templateObject = _taggedTemplateLiteral(['Hello ', ' world ', ''], ['Hello ', ' world ', '']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
 {
 	var a = 10;
 }
@@ -37,9 +43,9 @@
 	var _f = _ref[1];
 	var _ref2 = [1, 2, 3];
 	// 不完全解构
-	var x = _ref2[0];
-	var y = _ref2[1];
-	var foo = 'x';
+	var _x = _ref2[0];
+	var _y = _ref2[1];
+	var _foo = 'x';
 	//使用默认值，必须要写undefined而不能写其他的
 	var _undefined = undefined;
 	var bar = _undefined === undefined ? 'y' : _undefined;
@@ -48,11 +54,92 @@
 /*
 	4.对象解构赋值
 	与数组的区别：顺序不必相同，属性名必须相同
+	对一个已经声明的变量(对象)用于解构赋值要小心，见例3
 */
 {
 	var _a$b = { a: 1, b: 2 };
 	var a = _a$b.a;
 	var b = _a$b.b;
+	var _x2 = { x: 1 };
+	//这个默认值有点怪
+	var x = _x2.x;
+	var _x2$y = _x2.y;
+	var y = _x2$y === undefined ? 5 : _x2$y;
+	var _foo2 = { foo: 1 };
+	foo = _foo2.foo;
+}
+
+/*
+	5.字符串解构赋值
+*/
+{
+	var _hello = 'hello';
+
+	var _hello2 = _slicedToArray(_hello, 5);
+
+	var a = _hello2[0];
+	var b = _hello2[1];
+	var c = _hello2[2];
+	var d = _hello2[3];
+	var e = _hello2[4];
+}
+
+/*
+	6.函数参数的解构赋值
+
+	小结解构赋值用途:
+		* 交换变量的值
+		* 从函数返回多个值
+		* 函数参数的定义
+		* 提取JSON数据
+		* 遍历MAP结构
+		* 输入指定模块方法
+*/
+{
+	var add = function add(_ref3) {
+		var _ref32 = _slicedToArray(_ref3, 2);
+
+		var x = _ref32[0];
+		var y = _ref32[1];
+
+		return x + y;
+	};
+
+	add([1, 2]);
+}
+
+/*
+	7.字符串的扩展
+	可以用``来输入字符串模板，并且可以保留空格和缩进
+	``内可以调用变量${var}
+*/
+{
+	var a = '𠮷';
+	var s = '吉a';
+	var string = 'Hello world!';
+
+	String.fromCodePoint(0x20BB7);
+
+	s.codePointAt(0); // 134071
+
+	'abc'.at(0); // a
+	'吉林'.at(0); // 吉
+
+	string.startsWith('Hello');
+	string.endsWith('!');
+	string.includes('o');
+
+	'haha'.repeat(2); // 'hahahaha'
+}
+
+/*
+	8.标签模板
+*/
+{
+	var a = 5;
+	var b = 10;
+
+	tag(_templateObject, a + b, a * b);
 }
 
 // arrow function
